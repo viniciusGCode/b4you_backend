6b9babc30d059220ff6eca56262deefa84b4319b
viniciusGCode <ljonham@gmail.com>
2025-07-22 15:29:59 -0300

    Feat: Adding cors to localhost

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import productRoutes from '../modules/product/routes';
 import userRoutes from '../modules/user/routes';
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/products', productRoutes);
-
 app.use('/users', userRoutes);
 
 export default app;
